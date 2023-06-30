c703ea119eed5f1feb0ecf3f0f8528ddce735cf1
kay0829 <eluka97@daum.net>
2023-06-30 14:51:19 +0900

    💄 DESIGN 1) 폰트 추가 및 tailwindcss 커스텀 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Poppins, sans-serif"],
+                rancho: ["Rancho, cursive;"],
+            },
+            colors: {
+                primary: "#242424",
+                jungle: "#094b65",
+            },
+            translate: {
+                "reverse-2/4": "-50%",
+            },
+        },
     },
     plugins: [],
 };
