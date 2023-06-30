cb56e570a837f43fe5afea15181afad7bfb24655
kay0829 <eluka97@daum.net>
2023-06-30 14:54:46 +0900

    🔨 FEAT 1) 스크롤을 위한 Lorem 텍스트 영역 Lorem 컴포넌트 추가

---
+++ src/component/Sample/Lorem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Lorem() {
+    return (
+        <div className="relative p-24 h-screen bg-gradient-to-t from-white to-jungle">
+            <h2 className="text-4xl font-bold mb-2">Parallax Scrolling Effects</h2>
+            <p className="text-md">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta sit sed, sint placeat, hic alias facere
+                harum quos odit esse suscipit magnam qui aperiam consequuntur. Ex libero labore autem necessitatibus.
+            </p>
+        </div>
+    );
+}
+
+export default Lorem;
