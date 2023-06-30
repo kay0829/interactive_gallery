8db51d22c865c4026669f77fb267c0c2e7b571e8
kay0829 <eluka97@daum.net>
2023-06-30 11:32:25 +0900

    🔧 CHORE 0) 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@api", replacement: "/src/api" },
+            { find: "@asset", replacement: "/src/asset" },
+            { find: "@component", replacement: "/src/component" },
+            { find: "@container", replacement: "/src/container" },
+            { find: "@feature", replacement: "/src/feature" },
+            { find: "@hook", replacement: "/src/hook" },
+            { find: "@route", replacement: "/src/route" },
+            { find: "@style", replacement: "/src/style" },
+            { find: "@util", replacement: "/src/component" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
+});
